f620d8bd69be997257e40a42a972f6034a08579f
mucahiddayan <muecahid@dayan.one>
2017-10-25 11:27:07 +0200

    testing

--- vegan-rezept.php
+++ vegan-rezept.php
@@ -17,18 +17,19 @@ class VeganRezept {
     public function __construct(){
         $this-> pluginDirUrl = plugin_dir_url( __FILE__ );
         add_action( 'wp_enqueue_scripts', array( $this,'vegan_rezept_styles') );
+        echo $this-> pluginDirUrl;
     }
 
 
     /**
-     * 
+     * Style and JS Files is being embedded
      */
     private function vegan_rezept_styles(){
         
-        wp_enqueue_style( 'afs', $this->$pluginDirUrl .'css/style.css', '', true );
+        wp_enqueue_style( 'vegan_rezept_style', $this->$pluginDirUrl .'css/style.css', '', true );
         wp_enqueue_style('fontAwesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css', '3.2.1', 'all' );
         wp_register_script('vegan_rezept_js',$this->$pluginDirUrl . 'js/main.js', array(), '1.0', true );
-        wp_localize_script('vegan_rezept_js','pluginSettings',array(
+        wp_localize_script('vegan_rezept_js','veganRezept',array(
                                         'nonce'=>wp_create_nonce( 'wp_rest' ),
                                         'pluginDirUrl' => $this->pluginDirUrl
                                 )
