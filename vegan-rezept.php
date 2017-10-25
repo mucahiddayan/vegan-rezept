6c456724660f8ee39e8e9dcfb1d34c15c5a29b8f
mucahiddayan <muecahid@dayan.one>
2017-10-25 11:14:23 +0200

    it has to be enough

---
+++ vegan-rezept.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Vegan Rezept
+ * Plugin URI: https://mücahiddayan.com
+ * Description: Vegan Rezept
+ * Version: 1.0.0
+ * Author: Mücahid Dayan
+ * Author URI: https://mücahiddayan.com
+ * License: GPL2
+ */
+
+class VeganRezept {
+
+
+
+}
+
+$vegan_rezept = new VeganRezept();
+
+
+?>
