ab6f3b4418da350b9172dec00cefdf5f841e8c40
mucahiddayan <muecahid@dayan.one>
2017-11-10 14:45:11 +0100

    testing

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@
 */
 
 /**
- * 
- * @param {WP_Post} recipe
- * title required
- * @returns {id:number} of the just created recipe
- */
+* 
+* @param {WP_Post} recipe
+* title required
+* @returns {id:number} of the just created recipe
+*/
 let createRecipe = (recipe)=>{
     // if(!recipe.hasOwnProperty('title') || recipe.title == ''){
     if(!recipe.title){
@@ -24,13 +24,14 @@ let createRecipe = (recipe)=>{
         },
         body:JSON.stringify(recipe),
     })
-    .then(e=>e.json());
+    .then(e=>e.json())
+    .catch(err=>console.log(err));
 }
 
 /**
- * 
- * @param {WP_Post} recipe 
- */
+* 
+* @param {WP_Post} recipe 
+*/
 let editRecipe = (recipe)=>{
     if(!recipe.id || recipe.id == 0){
         console.warn('ID is required and it can not be 0');
@@ -45,14 +46,15 @@ let editRecipe = (recipe)=>{
         },
         body:JSON.stringify(recipe),
     })
-    .then(e=>e.json());
+    .then(e=>e.json())
+    .catch(err=>console.log(err));
 }
 
 /**
- * deletes recipe with given id
- * @param {number} id
- * @returns {Promise}
- */
+* deletes recipe with given id
+* @param {number} id
+* @returns {Promise}
+*/
 let deleteRecipe = (id)=>{
     if(!id || id == 0){
         console.warn('ID is required and it can not be 0');
@@ -66,27 +68,29 @@ let deleteRecipe = (id)=>{
             'Content-Type':'application/json'
         }
     })
-    .then(e=>e.json());
+    .then(e=>e.json())
+    .catch(err=>console.log(err));
 }
 
 /**
- * get all recipes
- * @returns {Promise}
- */
+* get all recipes
+* @returns {Promise}
+*/
 let getRecipes = ()=>{
     return fetch(`${location.origin}/wp-json/wp/v2/recipes`,{
         headers:{
             'Content-Type':'application/json'
         }
     })
-    .then(e=>e.json());
+    .then(e=>e.json())
+    .catch(err=>console.log(err));
 }
 
 /**
- * 
- * @param {number} id 
- * @returns {Promise}
- */
+* 
+* @param {number} id 
+* @returns {Promise}
+*/
 let getRecipe = (id)=>{
     if(!id){
         console.warn('ID is required');
@@ -97,16 +101,32 @@ let getRecipe = (id)=>{
             'Content-Type':'application/json'
         }
     })
-    .then(e=>e.json());
+    .then(e=>e.json())
+    .catch(err=>console.log(err));
+}
+
+let getRecipesFromMyBook = ()=>{
+    
+    fetch(veganRezept.pluginDirUrl+'ajax.php',{
+        method:'post',
+        headers:{
+            'Content-Type':'application/x-www-form-urlencoded',
+            'Accept':'application/php'
+        },
+        body:JSON.stringify({func:'get_recipes_from_my_book'})
+    })
+    .then(e=>e.json())
+    .then(e=>console.log(e))
+    .catch(err=>console.log(err))
 }
 
 /**
- * 
- * @param {number} start 
- * @param {number} end 
- * @param {number} range 
- * @returns {array<number>}
- */
+* 
+* @param {number} start 
+* @param {number} end 
+* @param {number} range 
+* @returns {array<number>}
+*/
 let createArray = (start=0,end=10,range=1)=>{
     var array = [];
     for(let i = start; i< end; i += range){
@@ -130,26 +150,26 @@ app.directive('recipes',()=>{
         scope.defaultImg = `${veganRezept.pluginDirUrl}/img/salad.png`;
         console.log(scope.recipes);
     }
-
+    
     let template = (el,attrs)=>{
         return `
         <div id="filter-wrapper">
         <input type="text" ng-model="search" placeholder="Suche nach Rezepten"/>
-    </div>
-    <div id="recipes-container">
-        <div class="recipe-wrapper" ng-ig="recipes.length" ng-repeat="recipe in recipes |filter:search track by $index">
-            <div class="recipe">
-                <div class="recipe-img">
-                    <img ng-attr-title="{{recipe.img_url?recipe.post_title:'Icons made by Smashicons from CC 3.0 BY'}}" ng-src="{{recipe.img_url?recipe.img_url:defaultImg}}" alt="{{recipe.post_title}}"/>
-                </div>
-                <a target="_blank" rel="nofollow" href="{{recipe.guid}}">
-                    <span title="{{recipe.post_title}}" ng-bind="recipe.post_title |excerpt:2"></span>
-                </a>
-            </div>
-            <h2 class="recipes-empty" ng-if="!recipes.length">Es gibt kein Rezept zu zeigen!</h2>
         </div>
-    </div>
-    `;
+        <div id="recipes-container">
+        <div class="recipe-wrapper" ng-ig="recipes.length" ng-repeat="recipe in recipes |filter:search track by $index">
+        <div class="recipe">
+        <div class="recipe-img">
+        <img ng-attr-title="{{recipe.img_url?recipe.post_title:'Icons made by Smashicons from CC 3.0 BY'}}" ng-src="{{recipe.img_url?recipe.img_url:defaultImg}}" alt="{{recipe.post_title}}"/>
+        </div>
+        <a target="_blank" rel="nofollow" href="{{recipe.guid}}">
+        <span title="{{recipe.post_title}}" ng-bind="recipe.post_title |excerpt:2"></span>
+        </a>
+        </div>
+        <h2 class="recipes-empty" ng-if="!recipes.length">Es gibt kein Rezept zu zeigen!</h2>
+        </div>
+        </div>
+        `;
     }
     return {
         restrict : 'E',
