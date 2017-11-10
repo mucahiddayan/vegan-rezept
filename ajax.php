cfdb9f216b4be2515594cf22e518bed917a93fa5
mucahiddayan <muecahid@dayan.one>
2017-11-10 13:55:06 +0100

    testing

--- ajax.php
+++ ajax.php
@@ -1,6 +1,8 @@
 <?php 
 require_once  $_SERVER["DOCUMENT_ROOT"]."/wp-load.php";
 
-if(isset($_POST['func'])){
-    echo $_POST['func'];
+if(isset($_POST["func"])){
+    echo $_POST["func"];
+}else{
+    echo 'hollaaa';
 }
