3c4f1b05a595e43edb79dddda5136fca86b3ec8b
mucahiddayan <muecahid@dayan.one>
2017-11-10 14:21:08 +0100

    testing

--- ajax.php
+++ ajax.php
@@ -3,6 +3,4 @@ require_once  $_SERVER["DOCUMENT_ROOT"]."/wp-load.php";
 
 if(isset($_POST["func"])){
     echo json_encode(array('output'=>$_POST['func']));
-}else{
-    echo json_encode(array('output'=>$_POST));
 }
